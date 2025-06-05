4dcd25251e09deb080d3f805cc599c6a28861fe3
mo3tazJ <en.mo3taz@gmail.com>
2025-06-05 17:20:05 +0300

    finalizing

--- 3_Getting_and_Cleaning_Data/Week 2/quiz2.R
+++ 3_Getting_and_Cleaning_Data/Week 2/quiz2.R
@@ -23,4 +23,18 @@ options(httr_oob_default = TRUE)
 # Get OAuth credentials
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
 
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
