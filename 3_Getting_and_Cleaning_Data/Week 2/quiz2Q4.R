db53da578abf20cc1a9b040de5f89188631c9c87
mo3tazJ <en.mo3taz@gmail.com>
2025-06-05 18:36:01 +0300

    Final

--- 3_Getting_and_Cleaning_Data/Week 2/quiz2Q4.R
+++ 3_Getting_and_Cleaning_Data/Week 2/quiz2Q4.R
@@ -1,3 +1,17 @@
 ## Getting and Cleaning Data Quiz
 
 ## Question 4
+## How many characters are in the 10th, 20th, 30th and 100th lines of HTML from this page:
+## http://biostat.jhsph.edu/~jleek/contact.html
+
+# Establish a connection to the page
+
+connection <- url("http://biostat.jhsph.edu/~jleek/contact.html")
+html_code <- readLines(connection)
+# Important !!! closing the connection
+close(connection)
+# counting char using nchar() Function
+c(nchar(htmlCode[10]), nchar(htmlCode[20]), nchar(htmlCode[30]), nchar(htmlCode[100]))
+
+# Answer: 
+# 45 31 7 25
