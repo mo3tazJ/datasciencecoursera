23c448717c8f225a850638dfe61e45c2c800d99d
mo3tazJ <en.mo3taz@gmail.com>
2025-06-05 18:07:19 +0300

    Q2 Final answering

--- 3_Getting_and_Cleaning_Data/Week 2/quiz2Q2.R
+++ 3_Getting_and_Cleaning_Data/Week 2/quiz2Q2.R
@@ -7,3 +7,16 @@ install.packages("sqldf")
 
 # Loading Te Package
 library("sqldf")
+
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+# Constructing Directory path
+dir_path <- paste0(getwd(),"/3_Getting_and_Cleaning_Data/Week 2")
+# File path
+f <- file.path(dir_path, "ss06pid.csv")
+# Download File from url
+download.file(url, f)
+# Reading to a data table
+acs <- data.table::data.table(read.csv(f))
+
+# Querying
+query1 <- sqldf("select pwgtp1 from acs where AGEP < 50")
