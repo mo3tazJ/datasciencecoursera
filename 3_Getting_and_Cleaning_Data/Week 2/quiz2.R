00989086f9d2d64dc19dc443b26db29dd7a55734
mo3tazJ <en.mo3taz@gmail.com>
2025-06-05 16:36:27 +0300

    configure the app

--- 3_Getting_and_Cleaning_Data/Week 2/quiz2.R
+++ 3_Getting_and_Cleaning_Data/Week 2/quiz2.R
@@ -5,3 +5,14 @@
 # install.packages("httpuv")
 # install.packages("httr")
 
+library(jsonlite)
+library(httpuv)
+library(httr)
+
+# OAuth endpoint
+oauth_endpoints("github")
+
+# Configure The app created in Github (Moutaz_Github_App)
+myapp <- oauth_app(appname = "Moutaz_Github_App",
+                   key = "Ov23liA4EjAgknVBUGrR",
+                   secret = "daa03bbab5ea4247f3c248a193419610a80fe0d1")
