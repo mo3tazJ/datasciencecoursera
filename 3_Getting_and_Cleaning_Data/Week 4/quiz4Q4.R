75894b203e0e0be8d08ec737fef8255cc1479d00
mo3tazJ <en.mo3taz@gmail.com>
2025-06-08 00:23:43 +0300

    final

--- 3_Getting_and_Cleaning_Data/Week 4/quiz4Q4.R
+++ 3_Getting_and_Cleaning_Data/Week 4/quiz4Q4.R
@@ -11,3 +11,14 @@ GDPrank <- data.table::fread('gdp.csv'
                              , select = c(1, 2, 4, 5)
                              , col.names=c("CountryCode", "Rank", "Country", "GDP")
 )
+
+file2url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(file2url, "stats.csv", method = "curl")
+
+eduDT <- data.table::fread("stats.csv")
+
+mergedDT <- merge(GDPrank, eduDT, by = 'CountryCode')
+
+answer <- mergedDT[grepl(pattern = "Fiscal year end: June 30;", mergedDT[, `Special Notes`]), .N]
+
+print(paste0("Answer: ", answer))
