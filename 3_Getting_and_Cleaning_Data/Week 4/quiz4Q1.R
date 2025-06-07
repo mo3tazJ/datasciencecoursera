2bd4d66d1060fd85fbe59795dc7caf61a67bf8ef
mo3tazJ <en.mo3taz@gmail.com>
2025-06-07 23:31:04 +0300

    download and load data

--- 3_Getting_and_Cleaning_Data/Week 4/quiz4Q1.R
+++ 3_Getting_and_Cleaning_Data/Week 4/quiz4Q1.R
@@ -2,3 +2,9 @@
 ## Week 4
 ## Question 1
 
+library("data.table")
+fileurl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileurl, "communities.csv", method = "curl")
+communities <- data.table::fread("communities.csv")
+
+splitted_names <- strsplit(names(communities), "wgtp")
