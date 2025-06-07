25ed4d70fa7e6119d2ccaef22b46da1fb407f4b5
mo3tazJ <en.mo3taz@gmail.com>
2025-06-07 23:33:22 +0300

    final

--- 3_Getting_and_Cleaning_Data/Week 4/quiz4Q1.R
+++ 3_Getting_and_Cleaning_Data/Week 4/quiz4Q1.R
@@ -8,3 +8,9 @@ download.file(fileurl, "communities.csv", method = "curl")
 communities <- data.table::fread("communities.csv")
 
 splitted_names <- strsplit(names(communities), "wgtp")
+
+element_123 <- splitted_names[[123]]
+
+print(element_123)
+
+## answer: ""   "15"
