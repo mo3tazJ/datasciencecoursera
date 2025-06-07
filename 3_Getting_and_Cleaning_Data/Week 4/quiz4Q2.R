8ab70adee3e655803cad2e4cee1104e2bbe93944
mo3tazJ <en.mo3taz@gmail.com>
2025-06-07 23:41:07 +0300

    downloading and assigning data

--- 3_Getting_and_Cleaning_Data/Week 4/quiz4Q2.R
+++ 3_Getting_and_Cleaning_Data/Week 4/quiz4Q2.R
@@ -2,6 +2,10 @@
 ## Week 4
 ## Question 2
 
+# Important Notes:
+# Removed the s from https to be compatible with windows computers. 
+# Skip first 5 rows and only read in relevant columns
+
 # library("data.table")
-fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+fileurl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 download.file(fileurl, "gdp.csv", method = "curl")
